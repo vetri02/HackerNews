a40f5ebec76d4c3c9f4e9d875a4c62b03bb102cb
Vetrichelvan <vjeyapalpandy@care.com>
2015-02-14 00:39:13 +0530

    Login screen Animation

--- HackerNews/ViewController.m
+++ HackerNews/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *dialogView;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+- (IBAction)loginButtonDidPress:(id)sender;
 
 @end
 
@@ -18,7 +21,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [self setupGradients];
+    //[self setupGradients];
     
     [self setNeedsStatusBarAppearanceUpdate];
     
@@ -29,23 +32,10 @@
     
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 /********* Back Buttn ***********/
 
 - (IBAction)unwindFromView: (UIStoryboardSegue *)sender {}
-
-/********* Status Bar ***********/
-
--(UIStatusBarStyle)preferredStatusBarStyle {
-    return UIStatusBarStyleLightContent;
-}
-
-/* Web View Load indicator */
 
 
 /*
@@ -53,13 +43,37 @@
  Setup gradients
  *******************************************************
  */
-- (void) setupGradients {
-    
-    CAGradientLayer *gradientLayer = [Gradient orangeGradient];
-    gradientLayer.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-    [self.view.layer insertSublayer:gradientLayer atIndex:0];
-    
-}
+//- (void) setupGradients {
+//    
+//    CAGradientLayer *gradientLayer = [Gradient orangeGradient];
+//    gradientLayer.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+//    [self.view.layer insertSublayer:gradientLayer atIndex:0];
+//    
+//}
 //*********************************************************************************************************
 
+- (IBAction)loginButtonDidPress:(id)sender {
+    //Animate Login Button
+    
+    [UIView animateWithDuration:0.1 animations:^{
+        self.loginButton.transform = CGAffineTransformMakeTranslation(10, 0);
+    } completion:^(BOOL finished){
+        //Step 2
+        [UIView animateWithDuration:0.1 animations:^{
+            self.loginButton.transform = CGAffineTransformMakeTranslation(-10, 0);
+        } completion:^(BOOL finished){
+            [UIView animateWithDuration:0.1 animations:^{
+                self.loginButton.transform = CGAffineTransformMakeTranslation(0, 0);
+            }];
+        }];
+    }];
+    
+    // animationWithDuration with Damping
+    [UIView animateWithDuration:0.7 delay:0 usingSpringWithDamping:0.5
+          initialSpringVelocity:0 options:0 animations:^{
+              [self.dialogView setFrame:CGRectMake(self.dialogView.frame.origin.x, self.dialogView.frame.origin.y, self.dialogView.frame.size.width, 320)];
+          } completion:^(BOOL finished){
+              
+          }];
+}
 @end
--- Podfile
+++ Podfile
@@ -2,4 +2,5 @@ source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '7.1'
 
 pod 'AFNetworking', '~> 2.0'
+pod 'Canvas'
 
