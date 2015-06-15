4a3e46aff4d645ce9792af205ceb7836dfbfe577
Dedunovich Svetlana <lumenela@gmail.com>
2015-06-15 16:55:48 +0300

    Updated progress hud presentation logic to reflect data download progress

--- HackerNews/HomeTableViewController.m
+++ HackerNews/HomeTableViewController.m
@@ -19,8 +19,8 @@
 @interface HomeTableViewController ()
 
 @property (nonatomic, strong) NSMutableArray *temporaryTop500StoriesIds;
-@property (nonatomic, strong) NSMutableArray *storyEventRefs;
 @property (nonatomic, strong) StoryTableViewCell *prototypeCell;
+@property (nonatomic, assign) NSInteger updatedStoryCounter;
 
 @end
 
@@ -35,70 +35,6 @@
     return self;
 }
 
-
-#pragma mark - FireBase API
-
-- (void)getTopStories {
-    Firebase *ref = [[Firebase alloc] initWithUrl:@"https://hacker-news.firebaseio.com/v0/"];
-    Firebase *topStories = [ref childByAppendingPath:self.datasourceName];
-
-    // Attach a block to read the data at our posts reference
-    [topStories observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
-        
-        //NSLog(@"%@", snapshot.value);
-        
-        self.temporaryTop500StoriesIds = [snapshot.value mutableCopy];
-        
-        [self getStoryDescriptionsUsingNewIDs:YES];
-    } withCancelBlock:^(NSError *error) {
-        NSLog(@"%@", error.description);
-    }];
-
-}
-
-- (void)getStoryDescriptionsUsingNewIDs:(BOOL)usingNewIDs{
-    
-    if(usingNewIDs){
-        for(NSNumber *itemNumber in self.temporaryTop500StoriesIds){
-            [self getStoryDataOfItem:itemNumber usingNewIDs:usingNewIDs];
-        }
-    }
-    else{
-    }
-    
-}
-
-- (void)getStoryDataOfItem:(NSNumber *)itemNumber usingNewIDs:(BOOL)usingNewIDs{
-    
-    //[[Crashlytics sharedInstance] crash];
-    
-    NSString *urlString = [NSString stringWithFormat:@"https://hacker-news.firebaseio.com/v0/item/%@",itemNumber];
-    
-    NSLog(@"%@", itemNumber);
-    
-    Firebase *storyDescriptionRef = [[Firebase alloc] initWithUrl:urlString];
-    
-    [self.storyEventRefs addObject:storyDescriptionRef];
-    [storyDescriptionRef observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
-        
-        if(snapshot.value != [NSNull null]){
-            [self.tableView beginUpdates];
-            [self.storiesArray addObject:snapshot.value];
-            NSInteger row = self.storiesArray.count - 1;
-            NSIndexPath *indexPath = [NSIndexPath indexPathForRow:row inSection:0];
-            [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-            [self.tableView endUpdates];
-        }
-
-        
-        
-        
-    } withCancelBlock:^(NSError *error) {
-        
-    }
-     ];
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -109,23 +45,15 @@
     
     self.tableView.rowHeight = UITableViewAutomaticDimension;
     self.tableView.estimatedRowHeight = 160.0;
+    self.tableView.scrollsToTop = YES;
     
     UINib *celllNib = [UINib nibWithNibName:@"StoryTableCellView" bundle:nil] ;
     [self.tableView registerNib:celllNib forCellReuseIdentifier:@"storyCell"];
     
     [self getTopStories];
     
-    HUD = [[MBProgressHUD alloc] initWithView:self.view];
-    HUD.detailsLabelText = self.loadMsg;
-    HUD.mode = MBProgressHUDModeDeterminate;
-    
-    [HUD showWhileExecuting:@selector(doSomeFunkyStuff) onTarget:self withObject:nil animated:YES];
-    
-
     [self.view addSubview:HUD];
     [HUD show:YES];
-    
-    self.tableView.scrollsToTop = YES;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -134,47 +62,73 @@
     self.title = self.navTitle;
 }
 
-- (void)doSomeFunkyStuff {
-    float progress = 0.0;
+#pragma mark - FireBase API
+
+- (void)getTopStories {
+    Firebase *ref = [[Firebase alloc] initWithUrl:@"https://hacker-news.firebaseio.com/v0/"];
+    Firebase *topStories = [ref childByAppendingPath:self.datasourceName];
+
+    HUD = [[MBProgressHUD alloc] initWithView:self.view];
+    HUD.detailsLabelText = self.loadMsg;
+    HUD.mode = MBProgressHUDModeDeterminate;
     
-    while (progress < 1.0) {
-        progress += 0.005;
-        HUD.progress = progress;
-        usleep(50000);
+    [topStories observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        self.temporaryTop500StoriesIds = [snapshot.value mutableCopy];
+        self.updatedStoryCounter = 0;
+        [self getStoryDescriptionsUsingNewIDs:YES];
+    } withCancelBlock:^(NSError *error) {
+        NSLog(@"%@", error.description);
+    }];
+}
+
+- (void)getStoryDescriptionsUsingNewIDs:(BOOL)usingNewIDs{
+    
+    if (usingNewIDs) {
+        for (NSNumber *itemNumber in self.temporaryTop500StoriesIds){
+            [self getStoryDataOfItem:itemNumber usingNewIDs:usingNewIDs];
+        }
     }
 }
 
-
-
+- (void)getStoryDataOfItem:(NSNumber *)itemNumber usingNewIDs:(BOOL)usingNewIDs{
+    
+    NSString *urlString = [NSString stringWithFormat:@"https://hacker-news.firebaseio.com/v0/item/%@",itemNumber];
+    
+    Firebase *storyDescriptionRef = [[Firebase alloc] initWithUrl:urlString];
+    
+    [storyDescriptionRef observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        if (snapshot.value != [NSNull null]) {
+            [self.storiesArray addObject:snapshot.value];
+        }
+        
+        self.updatedStoryCounter++;
+        [MBProgressHUD HUDForView:self.view].progress = (float)(self.updatedStoryCounter)/self.temporaryTop500StoriesIds.count;
+        if (self.updatedStoryCounter == self.temporaryTop500StoriesIds.count) {
+            [MBProgressHUD hideHUDForView:self.view animated:YES];
+            [self.tableView reloadData];
+        }
+    } withCancelBlock:^(NSError *error) {
+        
+    }];
+}
 
 #pragma mark - Table view data source
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    // Return the number of rows in the section.
-   
-    //NSLog(@"%@", snapshot.value);
     return [self.storiesArray count];
 }
-
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     StoryTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"storyCell" forIndexPath:indexPath];
     
-        if (cell==nil) {
-            
-            NSArray* storyObject = [[NSBundle mainBundle] loadNibNamed:@"CurrentOffersInfoView" owner:self options:nil];
-            
-            cell = [storyObject firstObject];
-        }
-    
-    
-    
+    if (cell == nil) {
+        NSArray *storyObject = [[NSBundle mainBundle] loadNibNamed:@"CurrentOffersInfoView" owner:self options:nil];
+        cell = [storyObject firstObject];
+    }
     
     // Get data from the array at position of the row
     NSDictionary *story = [self.storiesArray objectAtIndex:indexPath.row];
@@ -189,9 +143,7 @@
     NSString *commentCountText = [NSString stringWithFormat:@"%@",  @(commentCount)];
 
     NSURL* url = [NSURL URLWithString:[story valueForKey:@"url"]];
-    NSString* reducedUrl = [NSString stringWithFormat:
-                            @"%@",url.host];
-    //NSLog(@"Output is: \"%@\"", reducedUrl);
+    NSString* reducedUrl = [NSString stringWithFormat:@"%@",url.host];
     if([story valueForKey:@"deleted"]){
     } else {
         // Apply the data to each row
@@ -209,19 +161,15 @@
     }
     [HUD hide:YES];
     
-    //NSLog(@"Output is: \"%@\"", cell.titleLabel);
-    
     return cell;
 }
 
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     NSDictionary *story = [self.storiesArray objectAtIndex:indexPath.row];
         [self performSegueWithIdentifier:@"toWebView" sender:story];
 }
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Make sure your segue name in storyboard is the same as this line
     if ([[segue identifier] isEqualToString:@"toWebView"])
     {
